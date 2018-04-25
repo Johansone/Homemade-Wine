8a9a0e624f5ac0e75b7d5082211a717aee583042
Johansone <sintijajohansone@gmail.com>
2018-04-25 13:01:01 -0400

    Create final project

---
+++ script.js
@@ -0,0 +1,39 @@
+
+function showbottling() {
+    document.getElementById("bottling").style.display = "block";
+}
+function hidebottling() {
+    document.getElementById("bottling").style.display = "none";
+}
+
+function showsiphoning() {
+    document.getElementById("siphoning").style.display = "block";
+}
+function hidesiphoning() {
+    document.getElementById("siphoning").style.display = "none";
+}
+
+function show_recipe_Apple() {
+    document.getElementById("recipe_Apple").style.display = "block";
+    document.getElementById("recipe_Strawberry").style.display = "none";
+}
+
+function show_recipe_Strawberry() {
+    document.getElementById("recipe_Strawberry").style.display = "block";
+    document.getElementById("recipe_Apple").style.display = "none";
+}
+
+// Hamburger menu
+
+function show_hamburgermenu() {
+    document.getElementById("hamburgermenu").style.display = "block";
+}
+function show_types_of_wine() {
+    document.getElementById("Types_js").style.display = "block";
+}
+function show_useful_websites() {
+    document.getElementById("Websites_js").style.display = "block";
+}
+function hide_hamburgermenu() {
+    document.getElementById("hamburgermenu").style.display = "none";
+}
