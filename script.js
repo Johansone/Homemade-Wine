94bea3dbc8be90b37c0105a6825a5c45fd308bf1
Johansone <sintijajohansone@gmail.com>
2018-04-26 18:00:30 -0400

    corrections 2

--- script.js
+++ script.js
@@ -27,12 +27,14 @@ function show_recipe_Strawberry() {
 function show_recipe_Pear() {
     document.getElementById("recipe_Pear").style.display = "block";
     document.getElementById("recipe_Apple").style.display = "none";
-  document.getElementById("recipe_Strawberry").style.display = "none";
+    document.getElementById("recipe_Strawberry").style.display = "none";
 }
 // Hamburger menu
 
 function show_hamburgermenu() {
     document.getElementById("hamburgermenu").style.display = "block";
+    document.getElementById("Types_js").style.display = "none";
+    document.getElementById("Websites_js").style.display = "none";
 }
 function show_types_of_wine() {
     document.getElementById("Types_js").style.display = "block";
